3b5c622d752d5e7ba8d92b64b864cf34e890755e
Nap <null.lxd.pw@gmail.com>
2021-12-20 13:27:25 +0300

    qt/5.15.2: Reworking

--- qt-5.15.2/conanfile.py
+++ qt-5.15.2/conanfile.py
@@ -7,16 +7,30 @@ class QtConan(ConanFile):
     version = "5.15.2"
     settings = "os", "arch", "compiler", "build_type"
 
+    exports = [ "patches/*.diff" ]
+    patches = [ ]
+
+    no_copy_source = True
+
     def source(self):
-        git = tools.Git()
+        git = tools.Git(folder='qt5')
         git.clone('https://github.com/qt/qt5.git')
-        git.checkout('v5.15.2')
-        self.run('./init-repository')
+        git.checkout('v' + self.version)
+
+        with tools.chdir('qt5'):
+            self.run('./init-repository')
+
+            for patch in self.patches:
+                print(f"Applying patch '{patch}'...")
+                self.run(f'git apply ../patches/{patch}')
+
+            self.run('rm -rf qtwebglplugin qtvirtualkeyboard qtgamepad')
 
     def build_configure(self):
         args = [
-            '-prefix', self.package_folder,
+            '-prefix', os.path.join(self.build_folder, 'package'),
             '-xplatform', self.xplatform,
+            '-sdk iphoneos',
             '-skip qtwebengine',
             '-opensource',
             '-confirm-license',
@@ -31,24 +45,21 @@ class QtConan(ConanFile):
             args.append('-release')
 
         if self.settings.os == 'Android':
-            if self.settings.arch:
+            if self.settings.arch == 'x86_64':
+                args.append('-android-abis arm64-v8a,armeabi-v7a')
+            else:
                 args.append('-android-arch')
                 args.append('arm64-v8a' if self.settings.arch == 'armv8' else 'armeabi-v7a')
-            else:
-                args.append('-android-abis arm64-v8a,armeabi-v7a')
         else:
             args.append('-static')
 
-        self.run(f"{self.source_folder}/configure {' '.join(args)}")
+        self.run(f"{self.source_folder}/qt5/configure {' '.join(args)}")
 
     def build_make(self):
         self.run('make -j6')
-        self.run('make -j1 install')
+        self.run('make -j6 install')
 
     def build(self):
-        open('.qmake.stash', 'w').close()
-        open('.qmake.super', 'w').close()
-
         self.build_configure()
         self.build_make()
 
@@ -56,7 +67,7 @@ class QtConan(ConanFile):
         self.copy('*', src='package', dst='.', excludes='doc/*')
 
     def package_info(self):
-        self.cpp_info.builddirs = [os.path.join(self.package_folder)]
+        self.cpp_info.builddirs = [self.package_folder]
 
     @property
     def xplatform(self):
